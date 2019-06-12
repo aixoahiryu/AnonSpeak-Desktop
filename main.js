1c8de9917dffef231e2448d924105838d8cd0973
aixoahiryu <aixoahiryu@gmail.com>
2019-06-12 16:37:53 +0700

    Trying to make the UI feel more native

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ let win;
 
 function createWindow () {
   win = new BrowserWindow({
-    width: 1280,
+    width: 1200,
     height: 800,
     frame: false,
     titleBarStyle: 'hidden',
+    backgroundColor: '#312450',
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: false
     }
   })
 
   win.loadURL('http://localhost:7735')
   // win.webContents.openDevTools()
+  
+  // win.once('ready-to-show', () => {
+  //   win.show()
+  // })
+
   win.on('closed', () => {
     win = null
   })
